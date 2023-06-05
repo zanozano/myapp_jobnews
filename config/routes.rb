286a8797bb6a9e9c60feeb793159f6adfc4e05b1
zanozano <zanozanozano@gmail.com>
2023-06-05 16:17:02 +0700

    update4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+
   resources :jobs
   resources :profiles, only: [:show]
-  devise_for :users
   root "jobs#index"
 end
