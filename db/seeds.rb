fe386ef8a058e82ad489213f98116cb7b4b4947b
zanozano <zanozanozano@gmail.com>
2023-06-05 15:01:10 +0700

    styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,19 @@ User.create!(
   admin: true
 )
 
+#normal usuario
+User.create!(
+  name: 'Juan',
+  email: 'juan@job.com',
+  password: '123456',
+  password_confirmation: '123456',
+  admin: false
+)
+
+
 # Crea 10 empleos falsos
 10.times do
   title = Faker::Job.title
-  description = Faker::Lorem.paragraph(sentence_count: 2)
+  description = Faker::Lorem.paragraph(sentence_count: 4)
   Job.create(title: title, description: description)
 end
